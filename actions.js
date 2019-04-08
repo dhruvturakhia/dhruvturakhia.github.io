43c0795202702d9a942d831da6082e3d4f648e4c
dhruvturakhia <dhruvturakhia36@gmail.com>
2019-04-08 19:47:25 -0400

    changed the theme to seagreen and purple for better ui and changed the card designs for cooler responsiveness

--- actions.js
+++ actions.js
@@ -8,51 +8,61 @@ var projects = {
 	title: "My Projects",
 	info: [
 	{
-		img_src: "./landing-bg.jpg",
+		img_src: "./images/landing-bg.jpg",
 		skill_txt: "Skills",
 		name: "This is my project\
 		dont stop believing",
-		desc_detailed: "WAHHHHHHHHHHHH",
+		desc_detailed: "WA111111111",
 	}, {
-		img_src: "./landing-bg.jpg",
+		img_src: "./images/landing-bg.jpg",
 		skill_txt: "Skills",
 		name: "This is my project\
 		dont stop believing",
-		desc_detailed: "WAHHHHHHHHHHHH",
+		desc_detailed: "WA222222222",
 	}, {
-		img_src: "./landing-bg.jpg",
+		img_src: "./images/landing-bg.jpg",
 		skill_txt: "Skills",
 		name: "This is my project\
 		dont stop believing",
-		desc_detailed: "WAHHHHHHHHHHHH",
+		desc_detailed: "WA333333333333",
 	}, {
-		img_src: "./landing-bg.jpg",
+		img_src: "./images/landing-bg.jpg",
 		skill_txt: "Skills",
 		name: "This is my project\
 		dont stop believing",
-		desc_detailed: "WAHHHHHHHHHHHH",
+		desc_detailed: "WA44444444",
 	}, {
-		img_src: "./landing-bg.jpg",
+		img_src: "./images/landing-bg.jpg",
 		skill_txt: "Skills",
 		name: "This is my project\
 		dont stop believing",
-		desc_detailed: "WAHHHHHHHHHHHH",
+		desc_detailed: "WAH555555555",
 	}, {
-		img_src: "./landing-bg.jpg",
+		img_src: "./images/landing-bg.jpg",
 		skill_txt: "Skills",
 		name: "This is my project\
 		dont stop believing",
-		desc_detailed: "WAHHHHHHHHHHHH",
+		desc_detailed: "WA666666666666",
+	}, {
+		img_src: "./images/landing-bg.jpg",
+		skill_txt: "Skills",
+		name: "This is my project\
+		dont stop believing",
+		desc_detailed: "WA77777777777777777",
 	},
 	],
 };
 
-var project_elements = document.querySelectorAll("div.cus-card");
+var project_cards = document.querySelectorAll("div.cus-card");
 
+var card_modals = document.querySelectorAll("div.modal");
 
 var skills = ["HTML", "CSS", "JS",
 "React", "Postgres", "Node", "Mongo", "Python", 
 "AWS", "Docker"]
+
+var skill_elements = document.querySelectorAll("div.skill");
+
 
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -77,25 +87,22 @@ textContent = projects.title;
 document.querySelectorAll("div.introduction div.title")[0]
 
 
-for (var i = 0; i < project_elements.length; i++) {
-	project_elements[i].childNodes[1].childNodes[1].
+for (var i = 0; i < project_cards.length; i++) {
+	project_cards[i].childNodes[1].childNodes[1].
 		setAttribute("src", projects.info[i].img_src);
 
-	project_elements[i].childNodes[1].childNodes[3].
-		childNodes[1].textContent = projects.info[i].skill_txt;
-
-	project_elements[i].childNodes[5].childNodes[1].
-		textContent = projects.info[i].name;
-
-	project_elements[i].childNodes[9].childNodes[1].
-		childNodes[1].textContent = projects.info[i].name;
-
-	project_elements[i].childNodes[9].childNodes[1].
-		childNodes[3].textContent = projects.info[i].desc_detailed;
-
+	project_cards[i].childNodes[1].childNodes[3].
+		childNodes[3].textContent = projects.info[i].name;
 }
 
-skill_elements = document.querySelectorAll("div.skill");
+for (var i = 0; i < card_modals.length; i++) {
+	card_modals[i].childNodes[1].childNodes[3].
+	textContent = projects.info[i].desc_detailed;
+		
+	card_modals[i].childNodes[1].childNodes[1].
+		textContent = projects.info[i].name;
+}
+
  for (var i = 0; i < skill_elements.length; i++) {
  	skill_elements[i].textContent = skills[i];
  	switch (i%3) {
