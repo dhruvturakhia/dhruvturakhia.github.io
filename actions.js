64cf118491b19d04ef6733c44cd5ed1b57c5b712
dhruvturakhia <dhruvcrator@gmail.com>
2019-04-12 23:00:46 -0400

    Edited content and added images

--- actions.js
+++ actions.js
@@ -1,54 +1,96 @@
 var intro = {
-	title: "Yo!",
-	body: "This is the body of\
-	my Greeting lorem blah blah blah",
+	title: "Hey! I am Dhruv, Nice to meet you.",
+	body: "My journey to software began in my \
+		freshman year when I got to work on robots \
+		and programming them for a variety of tasks, \
+		making me curious about programming and \
+		helped me discover my passion to develop software. \
+		I am a self taught programmer and believe in \
+		continuously learning using my experiences to solve \
+		problems. I have worked on a variety of projects \
+		especially Web-Development (Full-Stack), Natural \
+		Language Processing and Machine Learning. My \
+		philosophy is to stay curious, keep learning, \
+		solve problems and take challenges head on.",
 };
 
 var projects = {
 	title: "My Projects",
 	info: [
 	{
-		img_src: "./images/landing-bg.jpg",
-		skill_txt: "Skills",
-		name: "This is my project\
-		dont stop believing",
-		desc_detailed: "WA111111111",
+		img_src: "./images/facial-detection-min.jpg",
+		skill_txt: "React, Node, Express, Postgre",
+		name: "Face Detection Web Application",
+		desc_detailed: "A web-app to help you \
+			detect faces in an image and keep a track \
+			of faces you have found. It tracks \
+			user-specific count of the faces detected \
+			by the images submitted. It involved \
+			designing of a sign-in page with validation \
+			from a Postgre database. The user page has \
+			a counter for the faces detected till date \
+			and an entry for the source of the image for \
+			which faces are to be detected using a \
+			Machine Learning API.",
 	}, {
-		img_src: "./images/landing-bg.jpg",
-		skill_txt: "Skills",
-		name: "This is my project\
-		dont stop believing",
-		desc_detailed: "WA222222222",
+		img_src: "./images/grid_monitoring-min.jpg",
+		skill_txt: "R, Node, Angular, Express, MongoDB, \
+			OpenVPN",
+		name: "Remote monitoring and \
+			fault prevention for grid inverters",
+		desc_detailed: "A web application that \
+			helps us pull data from remote solar power \
+			grids and analyze them to monitor for faults, \
+			and predict if trends to fault are observed. \
+			The webpage consisted of a user sign-in, \
+			dashboard to view current active girds and \
+			their statuses, specific grid page to show \
+			in detail performance (parameters like power, \
+			up-time, current etc) of the gird and an option \
+			to predict the data for upto a week. \
+			The prediction of this data used a time series \
+			algorithm ETS-STL to map the exponential and \
+			seasonal nature of solar power generation using R. \
+			The connection between the remote grids was setup \
+			using OpenVPN."
 	}, {
-		img_src: "./images/landing-bg.jpg",
-		skill_txt: "Skills",
-		name: "This is my project\
-		dont stop believing",
-		desc_detailed: "WA333333333333",
+		img_src: "./images/digit-recognition-min.png",
+		skill_txt: "Python, sklearn, pytorch, numpy",
+		name: "Handwritten Digit Recognition",
+		desc_detailed: "It is necessary to design and \
+			evaluate performance of machine learning models \
+			for tasks along with parameter tuning and \
+			regularization. This project \
+			compared the performance of two techniques for \
+			handwritten digit recognition. The data was \
+			compressed using Principal Component Analysis (PCA) \
+			and fed to an SVM where the performance was evaluated \
+			for different kernels (RBF, linear) and trade-off on \
+			varying the C parameter. A deep neural network was \
+			implemented using Convolutional Neural Network and the \
+			performance was evaluated for varying architectures \
+			both custom and pre-existing like LeNet. The \
+			performance was also evaluated for different \
+			activation functions, loss functions and dropout \
+			regularization.",
 	}, {
-		img_src: "./images/landing-bg.jpg",
-		skill_txt: "Skills",
-		name: "This is my project\
-		dont stop believing",
-		desc_detailed: "WA44444444",
-	}, {
-		img_src: "./images/landing-bg.jpg",
-		skill_txt: "Skills",
-		name: "This is my project\
-		dont stop believing",
-		desc_detailed: "WAH555555555",
-	}, {
-		img_src: "./images/landing-bg.jpg",
-		skill_txt: "Skills",
-		name: "This is my project\
-		dont stop believing",
-		desc_detailed: "WA666666666666",
-	}, {
-		img_src: "./images/landing-bg.jpg",
-		skill_txt: "Skills",
-		name: "This is my project\
-		dont stop believing",
-		desc_detailed: "WA77777777777777777",
+		img_src: "./images/machine-learning-min.jpg",
+		skill_txt: "Matlab",
+		name: "Face Recognition",
+		desc_detailed: "Implementing mathematical \
+			algorithms can be a tedious task especially \
+			for machine learning tasks. Here we \
+			reimplemented certain pattern \
+			classification algorithms from scratch \
+			on matlab and then used them to classify \
+			faces from a standard data set. The feature \
+			compression algorithm like Principal Component \
+			Analysis (PCA) and Fisher's Linear Discriminant \
+			Analysis (LDA) and classification algorithms like \
+			Naive Bayes and K-NN (k-nearest neighbours). The \
+			performance was also evaluated with varying degrees \
+			of feature compressions and larger neighbourhoods \
+			as well.",
 	},
 	],
 };
@@ -101,6 +143,9 @@ for (var i = 0; i < card_modals.length; i++) {
 		
 	card_modals[i].childNodes[1].childNodes[1].
 		textContent = projects.info[i].name;
+
+	card_modals[i].childNodes[1].childNodes[5].
+		textContent = "Skills: " + projects.info[i].skill_txt;
 }
 
  for (var i = 0; i < skill_elements.length; i++) {
